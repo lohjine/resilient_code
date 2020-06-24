cb40c05c407347c6bf550627c7ecbe493be46309
lohjine <66872975+lohjine@users.noreply.github.com>
2020-06-24 14:24:32 +0800

    clean up tests

--- tests/test_resilient_code.py
+++ tests/test_resilient_code.py
@@ -1,6 +1,5 @@
 from unittest import TestCase
 from resilient_code import resilient, Resilient
-import types
 
 def function_raise_exception():
     new_var = 1
@@ -13,7 +12,12 @@ def function_raise_exception_decorated(awd):
     raise ValueError
     return new_var
 
-function_raise_exception_decorated(awd=3)
+@resilient(reraise=False)
+def function_raise_exception_decorated_withoutreraise(awd):
+    new_var = 2
+    raise ValueError
+    return new_var
+
 
 @resilient(max_tries=1, custom_log_msg='custom log message test')
 def function_raise_exception_decorated_custom_args():
@@ -21,54 +25,10 @@ def function_raise_exception_decorated_custom_args():
     raise ValueError
     return new_var
 
-for attempt in Resilient(max_tries=2):
+for attempt in Resilient(max_tries=2, reraise=False):
     with attempt:
         print(0)
         raise ValueError
-        
-        
-import sys
-import traceback
-import inspect
-try:
-    1
-    raise ValueError('error')
-except Exception as e:
-    q = e
-    w =sys.exc_info()
-    
-    r= traceback.format_exception(*sys.exc_info())
-
-
-
-with Resilient():
-    new_var = 5
-    raise ValueError
-
-
-_local = set(locals().keys())
-_globa = set(globals().keys())
-
-
-try:
-    asd = 5
-    _r_local = set([i for i in locals().keys() if i[0]!='_'])
-    raise ValueError
-    sdf = 6
-except:
-    _var_dump = inspect.trace()[-1][0].f_locals
-    _var_dump = {k: v for k, v in _var_dump.items() if k[0] != '_' and type(v) not in (types.FunctionType, types.ModuleType, type)}
-    _f_local = set([i for i in _var_dump.keys() if i[0]!='_' and type(i)])
-    
-    _n_local = set([i for i in locals().keys() if i[0]!='_'])
-    
-    _wtff_local = set([i for i in _var_dump.keys() if i[0]!='_'])
-    
-_r_local.difference(_local)
-_f_local.difference(_local)
-_n_local.difference(_local)
-_wtff_local.difference(_local)
-    
 
 
 class TestGet(TestCase):
@@ -78,28 +38,28 @@ class TestGet(TestCase):
             self.assertTrue(False)
         except:
             self.assertTrue(True)
-        
+
     def test_decorated_function_custom_args(self):
         try:
             function_raise_exception_decorated_custom_args()
             self.assertTrue(False)
         except:
             self.assertTrue(True)
-        
+
     def test_adhoc_decorated_function(self):
         try:
             resilient(function_raise_exception)()
             self.assertTrue(False)
         except:
             self.assertTrue(True)
-        
+
     def test_adhoc_decorated_function_reraisefalse(self):
         try:
             resilient(function_raise_exception, reraise=False)()
             self.assertTrue(True)
         except:
             self.assertTrue(False)
-            
+
     def test_code_block(self):
         try:
             with Resilient():
@@ -109,7 +69,7 @@ class TestGet(TestCase):
             self.assertTrue(False)
         except:
             self.assertTrue(True)
-            
+
     def test_code_block_reraisefalse(self):
         try:
             with Resilient(reraise=False):
